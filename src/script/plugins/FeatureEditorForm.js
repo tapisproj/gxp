14e022e47482c80c20dc3e3bc2d963613d7fdd2e
Martins Linde <martins.linde@gmail.com>
2013-01-22 11:01:40 +0200

    do not show boxlabel for checkboxes enough with fieldlabel, accept value 1 as true

--- src/script/plugins/FeatureEditorForm.js
+++ src/script/plugins/FeatureEditorForm.js
@@ -142,7 +142,8 @@ gxp.plugins.FeatureEditorForm = Ext.extend(Ext.FormPanel, {
                     fieldCfg.value = this.feature.attributes[name];
                 }
                 if (fieldCfg.value && fieldCfg.xtype == "checkbox") {
-                    fieldCfg.checked = Ext.isBoolean(fieldCfg.value) ? fieldCfg.value : (fieldCfg.value === "true");
+                    fieldCfg.checked = Ext.isBoolean(fieldCfg.value) ? fieldCfg.value : (fieldCfg.value === "true" || fieldCfg.value == "1");
+                    fieldCfg.boxLabel = "";
                 }
                 if (fieldCfg.value && fieldCfg.xtype == "gxp_datefield") {
                     fieldCfg.value = new Date(fieldCfg.value*1000);
